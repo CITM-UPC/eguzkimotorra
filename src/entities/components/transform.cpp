f70bd804bff8135e32511c6bf521adecca8f24a1
Sergi PR <t3m1X@users.noreply.github.com>
2023-11-09 11:24:22 +0100

    Added functions to update transforms recursively

--- src/entities/components/transform.cpp
+++ src/entities/components/transform.cpp
@@ -1,4 +1,5 @@
 #include "transform.h"
+#include "entities/entity.h"
 #include "glm/glm.hpp"
 #include "glm/ext/quaternion_float.hpp"
 #include "glm/ext/matrix_transform.hpp"
@@ -6,20 +7,70 @@
 #include "glm/gtc/type_ptr.hpp"
 
 ComponentTransform::ComponentTransform(Entity *owner) : Component(owner, ComponentType::Transform) {
-    position = glm::vec3(0, 0, 0);
-    scale = glm::vec3(1, 1, 1);
-    rotation = glm::identity<glm::quat>();
+    position_ = glm::vec3(0, 0, 0);
+    scale_ = glm::vec3(1, 1, 1);
+    rotation_ = glm::identity<glm::quat>();
 }
 
 ComponentTransform::ComponentTransform(Entity *owner, glm::vec3 position, glm::vec3 scale, glm::quat rotation) :
-  Component(owner, ComponentType::Transform), position(position), scale(scale), rotation(rotation) {}
+  Component(owner, ComponentType::Transform), position_(position), scale_(scale), rotation_(rotation) {}
 
 ComponentTransform::~ComponentTransform() {}
 
-glm::mat4 ComponentTransform::transform_matrix() const {
-    glm::mat4 ret = glm::translate(glm::identity<glm::mat4>(), position);
-    glm::scale(ret, scale);
-    ret *= glm::mat4_cast(rotation);
+glm::vec3 ComponentTransform::position() const { return position_; }
+glm::vec3 ComponentTransform::scale() const { return scale_;}
+glm::quat ComponentTransform::rotation() const { return rotation_; }
+const glm::mat4& ComponentTransform::local() const { return local_; }
+const glm::mat4& ComponentTransform::global() const { return global_; }
 
-    return ret;
+void ComponentTransform::SetPosition(glm::vec3 position) {
+    position_ = position;
+    dirty_ = true;
+}
+
+void ComponentTransform::SetScale(glm::vec3 scale) {
+    scale_ = scale;
+    dirty_ = true;
+}
+void ComponentTransform::SetRotation(glm::quat rotation) {
+    rotation_ = rotation;
+    dirty_ = true;
+}
+
+void ComponentTransform::UpdateRecursively(glm::mat4 parent_global, bool dirty) {
+    dirty = dirty || dirty_;
+    if (dirty)
+        RecalculateTransforms(parent_global);
+
+    for (auto &child : owner_->children)
+        child->transform->UpdateRecursively(global_, dirty);
+
+}
+
+void ComponentTransform::RecalculateTransforms()  {
+    if (dirty_) { // We only need to update the local transform if this object is dirty
+        local_ = glm::translate(glm::identity<glm::mat4>(), position_);
+        glm::scale(local_, scale_);
+        local_ *= glm::mat4_cast(rotation_);
+    }
+
+    if (owner_) {
+        glm::mat4 global = owner_->transform->global();
+        global_ = global * local_;
+    }
+    else
+        global_ = local_;
+
+    dirty_ = false;
+}
+
+void ComponentTransform::RecalculateTransforms(glm::mat4 parent_global)  {
+    if (dirty_) { // We only need to update the local transform if this object is dirty
+        local_ = glm::translate(glm::identity<glm::mat4>(), position_);
+        glm::scale(local_, scale_);
+        local_ *= glm::mat4_cast(rotation_);
+    }
+
+    global_ = parent_global * local_;
+    dirty_ = false;
 }
--- src/entities/components/transform.h
+++ src/entities/components/transform.h
@@ -13,12 +13,29 @@ public:
     ComponentTransform(Entity* owner, glm::vec3 position, glm::vec3 scale, glm::quat rotation);
     ~ComponentTransform();
 
-    glm::mat4 transform_matrix() const;
+    // Getters and setters
+    glm::vec3 position() const;
+    glm::vec3 scale() const;
+    glm::quat rotation() const;
+    const glm::mat4& local() const;
+    const glm::mat4& global() const;
 
-public:
-    glm::vec3 position;
-    glm::vec3 scale;
-    glm::quat rotation;
+    void SetPosition(glm::vec3 position);
+    void SetScale(glm::vec3 scale);
+    void SetRotation(glm::quat rotation);
+
+    void UpdateRecursively(glm::mat4 parent_global, bool dirty = false);
+
+    void RecalculateTransforms();
+    void RecalculateTransforms(glm::mat4 parent_global);
+
+private:
+    glm::vec3 position_;
+    glm::vec3 scale_;
+    glm::quat rotation_;
+    glm::mat4 local_;
+    glm::mat4 global_;
+    bool dirty_;
 };
 
 #endif
